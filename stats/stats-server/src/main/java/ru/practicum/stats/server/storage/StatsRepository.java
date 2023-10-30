a273d6d346559f342a6a277413dec7446dabf131
ka5trulkin <ka5trulkin@yandex.by>
2023-10-30 23:38:06 +0300

    fix: code style

--- stats/stats-server/src/main/java/ru/practicum/stats/server/storage/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/server/storage/StatsRepository.java
@@ -15,14 +15,14 @@ import java.util.List;
 import static ru.practicum.stats.server.model.QStatsHit.statsHit;
 
 public interface StatsRepository extends JpaRepository<StatsHit, Long>, QuerydslPredicateExecutor<StatsHit> {
-    default List<Tuple> getTuplesToResponse(Predicate predicate, StatsHitFilter filter, EntityManager manager) {
+    default List<Tuple> getTuplesToResponse(Predicate condition, StatsHitFilter filter, EntityManager manager) {
         final OrderSpecifier<Long> hits = filter.isUnique() ? statsHit.ip.countDistinct().desc() : statsHit.ip.count().desc();
         return new JPAQuery<Tuple>(manager)
                 .select(statsHit.app,
                         statsHit.uri,
                         filter.isUnique() ? statsHit.ip.countDistinct() : statsHit.ip.count())
                 .from(statsHit)
-                .where(predicate)
+                .where(condition)
                 .groupBy(statsHit.app, statsHit.uri)
                 .orderBy(hits)
                 .fetch();
